a7c040f1abb68cfdd2d2fe1ff7c685edcdcba5b5
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 18:43:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,9 @@ class ApplicationController < Sinatra::Base
     if @user != nil
       session[:user_id] = @user.id
       redirect '/account'
-    else 
+    else
       redirect '/'
+    end
   end
 
   get '/account' do
